ae6f9ed86284123766cf148011d6740033327e81
Ayaka Yasuda <ayakayasuda23@gmail.com>
2022-05-25 20:47:38 -0700

    added sql that creates relational tables and joins tables

--- creatures.sql
+++ creatures.sql
@@ -1,3 +1,6 @@
+-- createdb -U postgres creaturesdb
+-- psql -U postgres creaturesdb < creatures.sql
+
 CREATE TABLE wizards(
     name character varying(50),
     power character varying(50)
@@ -11,6 +14,16 @@ CREATE TABLE elves(
 CREATE TABLE hobbits(
     name character varying(50),
     parsonality character varying(50)
+);
+
+CREATE TABLE allies(
+    wizard character varying(50),
+    elf character varying(50)
+);
+
+CREATE TABLE guardians(
+    elf character varying(50),
+    hobbit character varying(50)
 );
 
 INSERT INTO wizards(name, power)
@@ -31,3 +44,36 @@ VALUES
 ('Sam', 'brave'),
 ('Bilbo', 'greedy');
 
+INSERT INTO allies(wizard, elf)
+VALUES
+('Gandalf', 'Legolas'),
+('Gandalf', 'Arwen'),
+('Saruman', 'Elrond'),
+('Saruman', 'Legolas');
+
+INSERT INTO guardians(elf, hobbit)
+VALUES
+('Legolas', 'Frodo'),
+('Arwen', 'Sam'),
+('Elrond', 'Bilbo');
+
+
+-- select
+
+SELECT * FROM wizards;
+SELECT name, power FROM wizards;
+SELECT * FROM wizards, elves;
+
+SELECT * FROM elves ORDER BY speed;
+SELECT * FROM elves ORDER BY speed DESC;
+SELECT * FROM elves ORDER BY speed ASC;
+
+SELECT * FROM elves LIMIT 2;
+SELECT * FROM elves WHERE speed > 9;
+
+
+-- join
+
+SELECT * FROM allies;
+SELECT * FROM allies JOIN elves ON allies.elf = elves.name;
+SELECT * FROM guardians JOIN hobbits ON guardians.hobbit = hobbits.name JOIN elves ON guardians.elf = elves.name;
